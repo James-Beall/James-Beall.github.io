c6635e70718968ed9d12d5f4d62a9b74d58c70bf
James Beall <57947890+James-Beall@users.noreply.github.com>
2019-11-27 14:42:47 +0000

    Revert "Navbar Test"
    This reverts commit 016cdc143a60a301a4113d3622e43fbc9c211ce8.

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,23 +399,3 @@
 					});
 
 })(jQuery);
-
-$(document).ready(function() {
-
-  function toggleSidebar() {
-    $(".button").toggleClass("active");
-    $("main").toggleClass("move-to-left");
-    $(".sidebar-item").toggleClass("active");
-  }
-
-  $(".button").on("click tap", function() {
-    toggleSidebar();
-  });
-
-  $(document).keyup(function(e) {
-    if (e.keyCode === 27) {
-      toggleSidebar();
-    }
-  });
-
-});
